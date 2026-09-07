e5a78c9e3c16d88e0cbd3593f7b118583ce47b26
matchadaisuke <matchadaisuke@gmail.com>
2026-09-07 20:36:48 +0900

    Release v1.2.0-ja-sec4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.ambient.tvclock"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10203
-        versionName = "1.2.0-ja-sec3"
+        versionCode = 10204
+        versionName = "1.2.0-ja-sec4"
         buildConfigField("String", "SPOTIFY_CLIENT_ID", "\"$spotifyClientId\"")
         buildConfigField("String", "UNSPLASH_ACCESS_KEY", "\"$unsplashAccessKey\"")
         buildConfigField("String", "GOOGLE_OAUTH_CLIENT_ID", "\"$googleOauthClientId\"")
